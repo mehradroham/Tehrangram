6940af91f051612abc1ce115c16c84dcd844758d
mehrad <mohamadyaghoobi1997@gmail.com>
2024-08-21 13:51:47 +0330

    v 1.0

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,13 @@
+const Loader = () => (
+  <div className="flex-center w-full">
+    <img
+      src="/assets/icons/loader.svg"
+      alt="loader"
+      width={24}
+      height={24}
+      className="animate-spin  bg-yellow-500"
+    />
+  </div>
+);
+
+export default Loader;
---
+++ tailwind.config.js
@@ -0,0 +1,67 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      
+      },
+    },
+    extend: {
+      colors: {
+        'primary-500': '#D4AF37',
+        'primary-600': '#C5B358 ',
+        'secondary-500': '#FFB620',
+        'off-white': '#D0DFFF',
+        'red': '#FF5A5A',
+        'dark-1': '#000000',
+        'dark-2': '#D3BD76',//
+        'dark-3': '#2F2401',
+        'base':'2F2401',//
+        'dark-4': '#2F2401',
+        'light-1': '#2F2401',
+        'light-2': '#2F2401',
+        'light-3': '#2F2401',
+        'light-4': '#2F2401',
+      },
+      screens: {
+        'xs': '480px',
+      
+      },
+      width: {
+        '420': '420px',
+        '465': '465px',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
